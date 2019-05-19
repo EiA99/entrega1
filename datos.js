2a7a22920a75b0141f848f84dc4aecaae88bdbaa
Elias Avilez <eliasramos159@gmail.com>
2019-05-19 11:35:17 -0500

    first commit

---
+++ datos.js
@@ -0,0 +1,21 @@
+cursos: [
+    {
+        id: 1,
+        nombre: "Node JS",
+        duracion: 32,
+        valor: 0
+    },
+    {
+        id: 2,
+        nombre: "Ingles",
+        duracion: 64,
+        valor: 200000
+    },
+    {
+        id: 3,
+        nombre: "Bolsa de valores",
+        duracion: 32,
+        valor: 150000
+    }
+]
+module.exports = { cursos }
